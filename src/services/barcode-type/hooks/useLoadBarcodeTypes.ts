242668ac4a5eb11b7af8dffc562001fbe528d055
hsoderlind <henrik.soderlind@live.se>
2024-04-20 10:04:01 +0200

    wait to fetch barcode types until shopId is resolved

--- src/services/barcode-type/hooks/useLoadBarcodeTypes.ts
+++ src/services/barcode-type/hooks/useLoadBarcodeTypes.ts
@@ -5,7 +5,7 @@ import { useGetAllBarcodeTypes } from "./useGetAllBarcodeTypes";
 export const useLoadBarcodeTypes = () => {
 	const shopId = useShopStore((state) => state.shop.id);
 	const [queryKey, queryFn] = useGetAllBarcodeTypes(shopId);
-	const query = useQuery(queryKey, queryFn);
+	const query = useQuery(queryKey, queryFn, {enabled: !!shopId});
 
 	return query;
 }
