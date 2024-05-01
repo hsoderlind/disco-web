666a083a1211a9810f9407ea191190780d13888a
hsoderlind <henrik.soderlind@live.se>
2024-05-01 22:15:10 +0200

    prevent context menu

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './lib/application-builder/ApplicationBuilder';
 
+document.addEventListener('contextmenu', (e) => {
+	import.meta.env.PROD && e.preventDefault();
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
