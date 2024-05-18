38f398082bf9693b684423807aa4fe8191a6b8ba
hsoderlind <henrik.soderlind@live.se>
2024-05-18 21:42:33 +0200

    omitted state from options

--- src/hooks/useNavigate.ts
+++ src/hooks/useNavigate.ts
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import { NavigateOptions, useNavigate as useReactNavigate } from "react-router-dom"
 export const useNavigate = () => {
 	const _navigate = useReactNavigate();
-	const navigate = useCallback((to: string, title: string, options?: NavigateOptions) => _navigate(to, {...options, state: {title}}), [_navigate]);
+	const navigate = useCallback((to: string, title: string, options?: Omit<NavigateOptions, 'state'>) => _navigate(to, {...options, state: {title}}), [_navigate]);
 
 	return navigate;
 }
