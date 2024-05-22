25a02eea872d9e4d300e544abb143c5081a351ca
hsoderlind <henrik.soderlind@live.se>
2024-05-22 21:41:18 +0200

    hide discogs button if not in shop

--- src/services/discogs/components/DiscogsButton.tsx
+++ src/services/discogs/components/DiscogsButton.tsx
@@ -2,9 +2,16 @@ import { Skeleton } from 'antd';
 import { useCheckTokenExist } from '../auth/hooks/useCheckTokenExist';
 import { AuthButton } from './AuthButton';
 import { Toolpanel } from './Toolpanel';
+import { useParams } from 'react-router-dom';
+import { RouteParams } from '../../../types/common';
 
 export const DiscogsButton = () => {
+	const params = useParams<RouteParams>();
 	const { data, isLoading } = useCheckTokenExist();
+
+	if (!params.urlAlias) {
+		return null;
+	}
 
 	if (isLoading) {
 		return <Skeleton.Button active />;
