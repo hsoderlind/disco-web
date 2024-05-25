7d02c7314060ce017204f79c80cffb36b7755d83
hsoderlind <henrik.soderlind@live.se>
2024-05-25 16:24:05 +0200

    changed auth layout and added logo

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,7 +4,14 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout: FC = () => {
 	return (
 		<div className='auth-layout'>
-			<Outlet />
+			<div className='panel'>
+				<div className='logo'>
+					<img src='/assets/logo_white.svg' alt={import.meta.env.VITE_APP_NAME} />
+				</div>
+				<div className='form'>
+					<Outlet />
+				</div>
+			</div>
 		</div>
 	);
 };
